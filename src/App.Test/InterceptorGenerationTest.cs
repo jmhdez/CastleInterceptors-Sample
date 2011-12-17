4232c3b7f05118bc8ed126be6a83fe683613c48e
jmhdez <juan.hernandez.arroyo@gmail.com>
2011-12-17 13:07:57 +0100

    Refactored tests

--- src/App.Test/InterceptorGenerationTest.cs
+++ src/App.Test/InterceptorGenerationTest.cs
@@ -10,20 +10,26 @@ namespace App.Test
     [TestFixture]
     public class InterceptorGenerationTest
     {
+        private WindsorContainer container;
+        private List<string> traceMessages;
+
+        [SetUp]
+        public void Setup()
+        {
+            container = new WindsorContainer();
+
+            traceMessages = new List<string>();
+
+            var writer = new ConfigurableTraceWriter(
+                (msg, args) => traceMessages.Add(string.Format(msg, args)));
+
+            container.Register(Component.For<ITraceWriter>().Instance(writer));
+            container.Register(Component.For<TraceInterceptor>());
+        }
+
         [Test]
         public void Can_Create_An_Interceptor_From_Configuration()
         {
-            var container = new WindsorContainer();
-
-            var messages = new List<string>();
-
-            var writer = new ConfigurableTraceWriter(
-                (msg, args) => messages.Add(string.Format(msg, args)));
-
-            container.Register(Component.For<ITraceWriter>().Instance(writer));
-
-            container.Register(Component.For<TraceInterceptor>());
-
             container.Register(Component.For<ICalculator>()
                                    .ImplementedBy<Calculator>()
                                    .Interceptors(new InterceptorReference(typeof (TraceInterceptor)))
@@ -33,25 +39,14 @@ namespace App.Test
             var calculator = container.Resolve<ICalculator>();
             calculator.Sum(2, 3);
 
-            Assert.That(messages[0], Is.StringContaining("Enter: Sum"));
-            Assert.That(messages[1], Is.StringContaining("Exit: Sum"));
+            Assert.That(traceMessages[0], Is.StringContaining("Enter: Sum"));
+            Assert.That(traceMessages[1], Is.StringContaining("Exit: Sum"));
             
         }
 
         [Test]
         public void Can_Select_Intercepted_Methods_With_Generation_Hooks()
         {
-            var container = new WindsorContainer();
-
-            var messages = new List<string>();
-
-            var writer = new ConfigurableTraceWriter(
-                (msg, args) => messages.Add(string.Format(msg, args)));
-
-            container.Register(Component.For<ITraceWriter>().Instance(writer));
-
-            container.Register(Component.For<TraceInterceptor>());
-
             container.Register(Component.For<ICalculator>()
                                    .ImplementedBy<Calculator>()
                                    .Interceptors(new InterceptorReference(typeof(TraceInterceptor)))
@@ -62,14 +57,14 @@ namespace App.Test
             var calculator = container.Resolve<ICalculator>();
             calculator.Sum(2, 3);
 
-            Assert.That(messages, Is.Not.Empty);
+            Assert.That(traceMessages, Is.Not.Empty);
 
             // Since Mult is not going to be intercepted, no new message should be generated
-            messages.Clear();
+            traceMessages.Clear();
 
             calculator.Mult(2, 3);
 
-            Assert.That(messages, Is.Empty);
+            Assert.That(traceMessages, Is.Empty);
         }
     }
 }
